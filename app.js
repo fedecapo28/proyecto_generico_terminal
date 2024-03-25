c322a83aab132b6d1ad87df74ebe199743ad7d50
Erick <erick.nahuelpaiva.001@gmail.com>
2024-03-25 20:12:52 -0300

    ejemplo base de un proyecto de terminal con comentarios

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+//importacion de modulos/archivos
 const leer = require("prompt-sync")();
 
-console.log("Ingresa algo en la terminal");
-let infoingresada = leer();
-console.log("Ingresaste:", infoingresada);
+//Declaracion e inicializacion de constantes
+//Variables que no cambian de valor en tiempo de ejecucion
+//Usamos SCREAM_SNAKE_CASE como nomenclatura
+const PI = 3.141592653;
+
+/**
+ * Procedimiento(funcion sin return) principal del programa
+ */
+function main() {
+    //Declaracion e inicializacion de variables
+    let infoingresada = "def texto";
+
+    //Instrucciones del algoritmo
+    console.log("Ingresa algo en la terminal");//Escribir
+    infoingresada = leer();//Leer
+    console.log("Ingresaste", infoingresada, PI);//Escribir
+}
+
+
+//llamado al procedimiento(funcion)/algoritmo principal del programa
+main();
