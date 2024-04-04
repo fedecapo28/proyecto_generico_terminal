9a4ad8af2da9a855f91df456bcf6ea2e5a461c58
Erick-C3 <81482790+Erick-C3@users.noreply.github.com>
2024-04-04 12:43:42 -0300

    error

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
 const leer = require("prompt-sync")();
 
-
-let nombreIngresado = "Nombre no ingresado >:(";//estoy declarando e inicializando la variable
-
-console.log("Ingresa tu nombre:");
-
-nombreIngresado = leer();
-
-console.log("Hola", nombreIngresado);
