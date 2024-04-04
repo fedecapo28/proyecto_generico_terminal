2226c51388e01530afd3450b0cfe6865f7bbeaf1
Erick-C3 <81482790+Erick-C3@users.noreply.github.com>
2024-04-04 10:22:00 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,2 @@
-//importacion de modulos/archivos
 const leer = require("prompt-sync")();
 
-//Declaracion e inicializacion de constantes
-//Variables que no cambian de valor en tiempo de ejecucion
-//Usamos SCREAM_SNAKE_CASE como nomenclatura
-const PI = 3.141592653;
-
-/**
- * Procedimiento(funcion sin return) principal del programa
- */
-function main() {
-    //Declaracion e inicializacion de variables
-    let infoingresada = "def texto";
-
-    //Instrucciones del algoritmo
-    console.log("Ingresa algo en la terminal");//Escribir
-    infoingresada = leer();//Leer
-    console.log("Ingresaste", infoingresada, PI);//Escribir
-}
-
-
-//llamado al procedimiento(funcion)/algoritmo principal del programa
-main();
