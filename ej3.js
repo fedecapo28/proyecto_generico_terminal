005d375ed1ea59d16ef6b716e764d897c029543d
fede <fedeferreiro28@gmail.com>
2024-04-23 20:25:06 -0300

    hasta aca

--- ej3.js
+++
@@ -1,4 +0,0 @@
-const leer = require("prompt-sync")();
-console.log("colocar un numero");
-let numeroIngresada = leer ();
-console.log("sumar o restar 2 numeros", numeroIngresada)
---
+++ secuencial/ej3.js
@@ -0,0 +1,4 @@
+const leer = require("prompt-sync")();
+console.log("numero ingresado");
+let numeroIngresado = leer ();
+console.log("mostrar el doble por comsola", 2*  numeroIngresado);
